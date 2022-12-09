a21894c5e545b5ce9e045681c45e51ffd4a614c5
Robin Osburg <Robin.osburg@web.de>
2022-12-09 11:01:31 +0100

    added about me and fixed introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { IntroductionComponent } from './introduction/introduction.component';
+
 import { MYSkillsComponent } from './my-skills/my-skills.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IntroductionComponent,
     MYSkillsComponent,
-    MyWorkComponent
+    MyWorkComponent,
+    IntroductionComponent,
+    AboutMeComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
