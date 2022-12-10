2fa0b0d66da54103037e6c26710da22956c28b80
Robin Osburg <Robin.osburg@web.de>
2022-12-10 12:01:51 +0100

    feat: add mail input

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -8,15 +8,18 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 export class ContactMeComponent {
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('mailField') mailField!: ElementRef;
   @ViewChild('messageField ') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
 
   async sendMail() {
     console.log('Sending mail', this.myForm);
     let nameField = this.nameField.nativeElement;
+    let mailField = this.mailField.nativeElement;
     let messageField = this.messageField.nativeElement;
     let sendButton = this.sendButton.nativeElement;
     nameField.disabled = true;
+    mailField.disabled = true;
     messageField.disabled = true;
     sendButton.disabled = true;
 
@@ -26,6 +29,7 @@ export class ContactMeComponent {
 
     let fd = new FormData();
     fd.append('name', nameField.value);
+    fd.append('mail', mailField.value);
     fd.append('message', messageField.value);
 
 
@@ -39,6 +43,7 @@ export class ContactMeComponent {
 
     ),
     nameField.disabled = false;
+    mailField.disabled = false;
     messageField.disabled = false;
     sendButton.disabled = false;
   }
